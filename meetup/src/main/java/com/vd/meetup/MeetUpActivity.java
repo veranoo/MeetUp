b610288f529d8b0eeaba43b8fbe01a1a7596e551
veranoo <przemas2008@gmail.com>
2013-12-09 22:44:05 +0100

    Czysta aplikacja

---
+++ meetup/src/main/java/com/vd/meetup/MeetUpActivity.java
@@ -0,0 +1,14 @@
+package com.vd.meetup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeetUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meet_up);
+    }
+
+}
